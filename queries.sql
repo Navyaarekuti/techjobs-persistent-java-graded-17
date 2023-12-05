203d0cf156c42a06e2d23b88853a8da54041c7b9
Navyaarekuti <arekutinavya1@gmail.com>
2023-12-05 13:51:02 -0600

    checking again

--- queries.sql
+++ queries.sql
@@ -1,5 +1,10 @@
 --Part 1
---SELECT FIELDS FROM job;
+
+
+    --id int
+    --employer varchar(255)
+    --name varchar(255)
+    --skills varchar(255)
 
 --Part 2
 SELECT name FROM employer WHERE location = "St. Louis City";
